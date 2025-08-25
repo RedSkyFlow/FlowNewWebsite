d013dd0018543af0e50211200d6fe7f9506ac211
Adam Partis <adam@flowhq.co.za>
2025-08-25 02:32:31 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {},
-  experimental: {
-    // This setting allows cross-origin requests in development, which is necessary
-    // for some development environments like the one used here.
-    allowedDevOrigins: ["https://6000-firebase-studio-1748920676879.cluster-uf6urqn4lned4spwk4xorq6bpo.cloudworkstations.dev"],
-  },
 };
 
 module.exports = nextConfig;
