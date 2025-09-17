8d758ceb5bed3d1e2cfcc7525f5f513ead42cc06
RedSkyFlow <adam@flowhq.co.za>
2025-09-17 15:06:54 +0200

    Fix: Animated scroll ts error

--- src/components/shared/ScrollAnimatedSection.tsx
+++ src/components/shared/ScrollAnimatedSection.tsx
@@ -1,41 +1,41 @@
+/**
+ * Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-'use client'
+'use client';
 
-import { motion, type MotionProps } from 'framer-motion'
-import type { ReactNode, ElementType } from 'react';
-import { cn } from '@/lib/utils'
+import { motion, useInView } from 'framer-motion';
+import { useRef, type ElementType, type ReactNode } from 'react';
 
-interface ScrollAnimatedSectionProps extends MotionProps {
+type ScrollAnimatedSectionProps<T extends ElementType> = {
   children: ReactNode;
-  className?: string;
-  delay?: number;
-  as?: ElementType;
-}
+  as?: T;
+};
 
-export function ScrollAnimatedSection({ 
-  children, 
-  className, 
-  delay = 0, 
-  as: Component = 'div', // Default to 'div'
-  ...rest 
-}: ScrollAnimatedSectionProps) {
-  
-  const MotionComponent = motion.create(Component);
+export default function ScrollAnimatedSection<T extends ElementType = 'div'>({
+  children,
+  as,
+  ...rest
+}: ScrollAnimatedSectionProps<T> & Omit<React.ComponentPropsWithoutRef<T>, keyof ScrollAnimatedSectionProps<T>>) {
+  const Component = as || 'div';
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true, amount: 0.2 });
 
   return (
-    <MotionComponent
-      className={cn(className)}
-      initial={{ opacity: 0, y: 40 }}
-      whileInView={{ opacity: 1, y: 0 }}
-      viewport={{ once: true, amount: 0.2 }}
-      transition={{ 
-        duration: 0.8, 
-        delay,
-        ease: [0.25, 1, 0.5, 1] 
-      }}
-      {...rest}
-    >
+    <Component ref={ref} {...rest} style={{ opacity: isInView ? 1 : 0, transform: isInView ? 'none' : 'translateY(50px)', transition: 'all 0.5s cubic-bezier(0.17, 0.55, 0.55, 1) 0.2s' }}>
       {children}
-    </MotionComponent>
-  )
+    </Component>
+  );
 }
